077090a70e93678651952151e6d5ef59f69fb74d
om <singhom931@gmail.com>
2025-02-02 08:06:07 +0530

    fix

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, redirect, url_for, flash, request, session, jsonify, copy_current_request_context
-from ..extensions import db, User, Reference, Container, Cargo, cache
+from ..extensions import db, User
 from . import main
 
 @main.route('/')
