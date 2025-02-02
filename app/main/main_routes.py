c1b0f102e142478bc49bde6682bc783affdea96b
om <singhom931@gmail.com>
2025-02-02 06:06:27 +0530

    main route

--- app/main/main_routes.py
+++ app/main/main_routes.py
@@ -1,3 +1,7 @@
 from flask import render_template, redirect, url_for, flash, request, session, jsonify, copy_current_request_context
 from ..extensions import db, User, Reference, Container, Cargo, cache
 from . import main
+
+@main.route('/')
+def index():
+    return render_template('main/home.html')
